860f6903e02286158c91b229169ee363ac692891
Mark Day <mday@mac.com>
2017-01-30 09:49:58 -0700

    Basics of simulation, include all-stock simulation
    * marketData is now a global (so it doesn’t pollute printing state of a given simulation object)
    * Added exception InsufficientFunds
    * class Simulation implements the basics of simulating a period of retirement, including printing state from InsufficientFunds
    * Added class AllStock, where the portfolio is entirely in stock

--- StockMarket.py
+++ StockMarket.py
@@ -4,8 +4,11 @@
 
 import sys, os
 
+class InsufficientFunds(Exception):
+	pass
+
 class MarketData(dict):
-	def __init__(self, path="~/StockMarketData.csv"):
+	def __init__(self, path="StockMarketData.csv"):
 		minYear = None
 		maxYear = None
 		filename = os.path.expanduser(path)
@@ -35,19 +38,59 @@ class MarketData(dict):
 				self[(year,month)] = (price, dividend, earnings)
 		self.minYear = minYear
 		self.maxYear = maxYear
+marketData = MarketData()
 
-class Simulation:
-	def __init__(self, marketData, years=30):
-		self.marketData = marketData
+class Simulation(object):
+	initialBalance = 1000000.00
+	
+	def __init__(self, years=30, rate=0.04):
 		self.years = years
-		
+		self.withdrawal = self.initialBalance * rate
+	
+	def __repr__(self):
+		return "{0}{1}".format(self.__class__.__name__, self.__dict__)
+	
+	def SimPeriod(self, startYear):
+		self.startYear = startYear
+		self.balance = self.initialBalance
+		self.cash = 0
+		self.stock = self.initialBalance
+		self.shares = self.stock / marketData[(startYear-1,12)][0]
+		self.SimInit()		# Give the algorithm a chance to initialize itself
+		for year in xrange(startYear, startYear+self.years):
+			assert self.cash >= 0.0
+			assert self.shares >= 0.0
+			self.year = year
+			self.SimYear()	# Move forward a year in the simulation
+	
+	def SimInit(self):
+		pass
+	
+	def SimYear(self):
+		pass
+	
 	def run(self):
-		for startYear in xrange(self.marketData.minYear, self.marketData.maxYear-self.years+2):
-			print startYear
+		failures = 0
+		for startYear in xrange(marketData.minYear+1, marketData.maxYear-self.years+2):
+			try:
+				self.SimPeriod(startYear)
+			except InsufficientFunds as ex:
+				print ex
+				failures += 1
+		if failures != 0:
+			print "{0}: {1} failed periods".format(self.__class__.__name__, failures)
+
+class AllStock(Simulation):
+	def SimYear(self):
+		price = marketData[(self.year,12)][0]
+		sellShares = self.withdrawal / price
+		if sellShares > self.shares:
+			raise InsufficientFunds(self)
+		self.shares -= sellShares
+		self.stock = self.balance = self.shares * price
 
 def main():
-	marketData = MarketData()
-	Simulation(marketData).run()
+	AllStock().run()
 	
 if __name__ == "__main__":
 	main()
